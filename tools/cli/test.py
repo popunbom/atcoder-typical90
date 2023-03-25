64db4945887bc243eaff5edcc5cc2fe25ac5f9fb
popunbom <28749104+popunbom@users.noreply.github.com>
2023-03-25 13:31:19 +0900

    Fix templates

--- tools/cli/test.py
+++ tools/cli/test.py
@@ -20,6 +20,7 @@ console = Console()
 
 class Args(argparse.Namespace):
     project_dir: str
+    only_exec: bool
     exec_name: str
     sample_dirname: str
     input_filename_pattern: str
@@ -40,28 +41,47 @@ def main(args: Args) -> int:
 
     all_ok = True
     for input_file, output_file in zip(input_files, output_files):
-        expect: str = None
-        with open(output_file) as f:
-            expect = f.read().strip()
+        input: str = None
+        with open(input_file) as f:
+            input = f.read().strip()
+        input = "\n".join(line.strip() for line in input.splitlines())
 
+        output: str = None
+        with open(output_file) as f:
+            output = f.read().strip()
+        output = "\n".join(line.strip() for line in output.splitlines())
+
+        stdout: str = None
         result: sp.CompletedProcess[bytes] = None
         with open(input_file, "rb") as f:
             result = sp.run(
                 path.abspath(path.join(args.project_dir, args.exec_name)),
-                input=f.read(),
+                input=input.encode(),
                 stdout=sp.PIPE
             )
-        assert result.returncode == 0
+            assert result.returncode == 0
+            stdout = result.stdout.decode().strip()
+        stdout = "\n".join(line.strip() for line in stdout.splitlines())
 
-        actual = result.stdout.decode().strip()
+        if args.only_exec:
+            console.print("\n".join([
+                "=== INPUT ===",
+                input,
+                "=== OUTPUT ===",
+                stdout,
+                "=== EXPECT ===",
+                output,
+                "",
+            ]))
+            continue
 
-        if expect != actual:
+        if output != stdout:
             console.print("\n".join([
                 "[bold red]NG: {!r}[/]".format(
                     path.relpath(input_file),
                 ),
-                "  Expect: {!r}".format(expect),
-                "  Actual: {!r}".format(actual),
+                "  Expect: {!r}".format(output),
+                "  Actual: {!r}".format(stdout),
             ]))
             all_ok = False
         else:
@@ -80,6 +100,8 @@ if __name__ == "__main__":
     parser.add_argument("project_dir", type=str,
                         help="Project directory")
 
+    parser.add_argument("--only_exec", required=False, action="store_true",
+                        help="Only exec (not testing)")
     parser.add_argument("--exec_name", required=False, type=str,
                         default=DEFAULTS["EXEC_NAME"],
                         help="Executable name")
